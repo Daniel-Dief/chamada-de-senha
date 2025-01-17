309bdb90a9bf3e19c5ba1a30b5cb3f92875c1113
HigorRodrigues <estevaohigor275@gmail.com>
2025-01-17 14:54:13 -0300

    Load screen function

---
+++ public/scripty.js
@@ -0,0 +1,27 @@
+// Função para carregar uma nova página dinamicamente
+function LP(file) {
+    fetch(file)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao carregar a página');
+        }
+        return response.text();
+      })
+      .then(html => {
+        document.documentElement.innerHTML = html;
+  
+        const newScript = document.createElement('script');
+        newScript.innerHTML = `
+          function LP(file) {
+            fetch(file)
+              .then(response => response.text())
+              .then(html => document.documentElement.innerHTML = html)
+              .catch(err => console.error(err));
+          }
+        `;
+        document.body.appendChild(newScript);
+      })
+      .catch(error => {
+        alert(`Erro ao carregar a página: ${error.message}`);
+      });
+  }
