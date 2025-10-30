44cf2fe990e8a33b45aac40319e4e3ca64f5419c
nickpolo99 <pixnickpolo@gmail.com>
2025-10-30 19:33:44 -0300

    rotas para backend

--- public/action.js
+++ public/action.js
@@ -1,8 +1,34 @@
-function increaseCounter(counterId) {
-    let counter = document.getElementById(counterId);
-    let value = parseInt(counter.innerText);
-    if (value < 99) {
-        counter.innerText = value + 1;
+async function increaseCounter(counterId) {
+    let url = '';
+
+    if (counterId === 'counter1') {
+        url = '/addPrimaryPassword';
+    } else if (counterId === 'counter2') {
+        url = '/addSecondaryPassword';
+    } else {
+        return;
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro na resposta do servidor');
+        }
+
+        const data = await response.json();
+        const counterElement = document.getElementById(counterId);
+
+        if (counterId === 'counter1') {
+            counterElement.innerText = data.primaryNumber;
+        } else if (counterId === 'counter2') {
+            counterElement.innerText = data.secondayNumber;
+        }
+
+    } catch (error) {
+        console.error('Falha na requisição:', error);
     }
 }
 
